0092d6e5415e77be5b2782551e69be2aea3d1286
Developertaposroy <taposroy616@gmail.com>
2023-04-30 12:14:46 +0600

    quiz

---
+++ Quiz-App/js/question.js
@@ -0,0 +1,93 @@
+let questions = [
+    {
+        numb:1,
+        question:'What Does HTML Stand For?',
+        answer:'Hyper Text Markup Language',
+        options:[
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: " border-spacing is given in _______ ",
+        answer: "pixels",
+        options: [
+          "pixels",
+          "cm",
+          "mm",
+          "inch "
+        ]
+      },
+      {
+        numb: 3,
+        question: "What Does PHP Stand For?",
+        answer: "Hypertext Preprocessor",
+        options: [
+          "Hypertext Preprocessor",
+          "Hypertext Programming",
+          "Hypertext Preprogramming",
+          "Hometext Preprocessor"
+        ]
+      },
+    
+      {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+          "Stylish Question Language",
+          "Stylesheet Query Language",
+          "Statement Question Language",
+          "Structured Query Language"
+        ]
+      },
+      {
+        numb: 5,
+        question: "How to express (Class) selector in CSS?",
+        answer: ".rahman",
+        options: [
+          "rahman",
+          " !rahman ",
+          "“rahman",
+          ".rahman"
+        ]
+      },
+      {
+        numb: 6,
+        question: "Find out the correct table row defining tag.",
+        answer: "tr",
+        options: [
+          "th",
+          "td",
+          "table",
+          "tr"
+        ]
+      },
+      {
+        numb: 7,
+        question: "Which CSS is written another CSS  file?",
+        answer: "external css",
+        options: [
+          " inline css ",
+          "external css",
+          " internal css ",
+          "none of them"
+        ]
+      },
+
+      {
+        numb: 8,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+          "eXtensible Markup Language",
+          "eXecutable Multiple Language",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+      
+]
